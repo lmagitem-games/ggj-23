5e96b91a2bc68200897f4e83eba809dff8eb292b
Luc Toupense <luc.toupense@itesoft.com>
2023-02-04 09:47:31 +0100

    Progress on map

--- src/scenes/main-scene.ts
+++ src/scenes/main-scene.ts
@@ -1,23 +1,50 @@
+export const TILE_WIDTH = 128;
+export const TILE_HEIGHT = 128;
+
 export class MainScene extends Phaser.Scene {
     constructor() {
         super({ key: "MainScene" });
     }
 
     preload() {
-        this.load.tilemapCSV('map', 'assets/levels/example.csv');
-        this.load.image("tiles1", "assets/tilemaps/medievalTiles.png");
-        this.load.image("tiles2", "assets/tilemaps/towerDefenseTiles.png");
+        this.load.tilemapCSV('background', 'assets/levels/example-l0.csv');
+        this.load.tilemapCSV('foreground', 'assets/levels/example-l1.csv');
+        this.load.image("tiles", "assets/tilemaps/medievalTiles.png");
     }
 
     create() {
-        // To use a map with an array
-        // const dataMap = [[0,1,2], [17,18,19]];
-        // const assetMap = this.make.tilemap({ data: dataMap, tileWidth: 128, tileHeight: 128 });
+        const tilesMultiplier: number = 1; // Génère une map plus ou moins grande
+        const mapWidth = tilesMultiplier * 16; // 64
+        const mapHeight = tilesMultiplier * 9; // 36
 
         // To use a level map made in CSV, the "key" corresponds to the csv tilemap key
-        const assetMap = this.make.tilemap({ key: "map", tileWidth: 128, tileHeight: 128 });
-        assetMap.addTilesetImage("tiles1");
-        const layer = assetMap.createLayer(0, "tiles1", 0, 0);
+        const background = this.make.tilemap({ key: "background", tileWidth: TILE_WIDTH, tileHeight: TILE_HEIGHT });
+        background.addTilesetImage("tiles");
+        const layerZero = background.createLayer(0, "tiles", 0, 0);
+        // const foreground = this.make.tilemap({ key: "foreground", tileWidth: TILE_WIDTH, tileHeight: TILE_HEIGHT });
+        // foreground.addTilesetImage("tiles");
+        // const layerOne = foreground.createLayer(1, "tiles", 0, 0);
+
+        this.cameras.main.setBounds(0, 0, TILE_WIDTH * mapWidth, TILE_HEIGHT * mapHeight);
+        this.cameras.main.centerOn(TILE_WIDTH * mapWidth / 2, TILE_HEIGHT * mapHeight / 2);
+
+        switch (tilesMultiplier) {
+            case 1:
+                this.cameras.main.setZoom(0.1042);
+                break;
+            case 2:
+                this.cameras.main.setZoom(0.1042);
+                break;
+            case 3:
+                this.cameras.main.setZoom(0.1042);
+                break;
+            case 5:
+                this.cameras.main.setZoom(0.1042);
+                break;
+            default:
+                this.cameras.main.setZoom(0.1042);
+                break;
+        }
     }
 
     update() {
